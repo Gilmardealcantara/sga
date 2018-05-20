a3b10b4876575a6b36e3369fe6964345ed9fc1a1
gilmar <gilmardealcantara@gmail.com>
2018-05-20 20:43:06 -0300

    Enable JPA Auditing

--- src/main/java/com/sga/api/SgaApplication.java
+++ src/main/java/com/sga/api/SgaApplication.java
@@ -2,8 +2,10 @@ package com.sga.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  
 @SpringBootApplication
+@EnableJpaAuditing
 public class SgaApplication {
 
 	public static void main(String[] args) {
