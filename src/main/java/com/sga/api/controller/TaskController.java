490fdf8b9558d0fc324e6d29aa5df88e55969253
gilmar <gilmardealcantara@gmail.com>
2018-05-20 20:49:06 -0300

    Creating TaskController

---
+++ src/main/java/com/sga/api/controller/TaskController.java
@@ -0,0 +1,66 @@
+package com.sga.api.controller;
+
+import com.sga.api.exception.ResourceNotFoundException;
+import com.sga.api.model.Task;
+import com.sga.api.repository.TaskRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    // Get All Tasks
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    // Create a new Task
+    @PostMapping("/task")
+    public Task createTask(@Valid @RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    // Get a Single Task
+    @GetMapping("/tasks/{id}")
+    public Task getTaskById(@PathVariable(value = "id") Long taskId) {
+        return taskRepository.findById(taskId)
+                .orElseThrow(() -> new ResourceNotFoundException("Task", "id", taskId));
+    }
+
+    // Update a Task
+    @PutMapping("/tasks/{id}")
+    public Task updateTask(@PathVariable(value = "id") Long taskId,
+                                            @Valid @RequestBody Task taskDetails) {
+
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new ResourceNotFoundException("Task", "id", taskId));
+
+        task.setTitle(taskDetails.getTitle());
+        task.setContent(taskDetails.getContent());
+        task.setStatus(taskDetails.getStatus());
+        
+        Task updatedTask = taskRepository.save(task);
+        return updatedTask;
+    }
+
+    // Delete a Task
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable(value = "id") Long taskId) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new ResourceNotFoundException("Task", "id", taskId));
+
+        taskRepository.delete(task);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
